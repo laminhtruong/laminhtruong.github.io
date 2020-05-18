7797a55b9659ea183e5010660aa81070107b0ed4
laminhtruong <laminhtruong@gmail.com>
2020-05-18 11:15:50 +0700

    test cocos creator

---
+++ cocos-creator/src/project.js
@@ -0,0 +1,13 @@
+System.register("chunks:///_virtual/prerequisite-imports:main",[],(function(e,t){return{setters:[],execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports:main', 'chunks:///_virtual/prerequisite-imports:main'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    var _m;
+    return {
+        setters: [function(m) { _m = m; }],
+        execute: function () { _export(_m); }
+    };
+    });
+});
