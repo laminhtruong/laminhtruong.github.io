fb685c205d3922472803fda2909665cd9d548fd3
laminhtruong <laminhtruong@gmail.com>
2020-06-07 13:47:56 +0700

    Update

--- flappy-bird/main.170d8.js
+++ flappy-bird/main.170d8.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.82b24.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.ed8aa.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
