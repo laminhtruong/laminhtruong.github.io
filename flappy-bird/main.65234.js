0a8870fb1fbdd6e13cbe05ee7b82a55aa63f4045
laminhtruong <laminhtruong@gmail.com>
2020-06-12 23:01:02 +0700

    Update

--- flappy-bird/main.65234.js
+++ flappy-bird/main.65234.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.530d8.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.344ad.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -179,7 +179,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.59db9.js');
+        require('src/settings.f631e.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -187,7 +187,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.59db9.js');
+        require('src/settings.f631e.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
