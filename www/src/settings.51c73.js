2e9dd0165ab7075688d6abd47d591d30fc70e9f1
laminhtruong <laminhtruong@gmail.com>
2020-06-28 21:29:29 +0700

    Update

--- www/src/settings.51c73.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","ui"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"",jsList:[],bundleVers:{internal:"ca8e7",main:"7bd9a"}};
