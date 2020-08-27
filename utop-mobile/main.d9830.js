53e0e40fa61f255e8a303e535e18f0d59edae3f3
laminhtruong <laminhtruong@gmail.com>
2020-08-27 22:58:06 +0700

    - Update

--- utop-mobile/main.d9830.js
+++ utop-mobile/main.d9830.js
@@ -119,7 +119,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.eb5a7.js');
+        require('src/settings.9e461.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -127,7 +127,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.eb5a7.js');
+        require('src/settings.9e461.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
