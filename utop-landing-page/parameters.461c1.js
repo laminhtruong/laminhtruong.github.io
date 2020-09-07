e8f1f898d670763cc3fc7c1842cf0e0f51e8db56
laminhtruong <laminhtruong@gmail.com>
2020-09-07 21:37:05 +0700

    - Update

---
+++ utop-landing-page/parameters.461c1.js
@@ -0,0 +1,50 @@
+var userParams = {};
+userParams.help = `- Tải app Utop và đăng kí tài khoản
+- Dùng Camera trên app để quét mã QR trên màn hình TV
+- Nhấn nút “Bắt đầu” trên điện thoại để chơi
+
+<b>Cách chơi:</b>
+- Người chơi đứng vào vị trí định sẵn
+- Di chuyển người lên và xuống điều khiển nhân vật trên màn hình tivi để thu thập thức ăn và tránh các chướng ngại vật.
+
+<b>Thời gian:</b> từ ngày 15/9/2020 đến ngày 29/9/2020
+
+<b>Giải thưởng:</b>
+49 điểm Utop để trải nghiệm nhiều tiện ích và ưu đãi tại các cửa hàng trong Trung Tâm Mua Sắm Vạn Hạnh Mall.
+<i>* Mỗi tài khoản chỉ được nhận thưởng 1 lần</i>
+
+<b>Thể lệ trò chơi:</b>
+- Bạn sẽ thắng khi đạt 600 điểm trong 1 lượt chơi
+- Thu thập đồ ăn để nhận được điểm
+- Bạn sẽ thua nếu va trúng “Tên lửa” hoặc “Con dơi”. Vậy nên cẩn thận nhé!
+- Mỗi người có <b>3 lượt chơi</b>
+- Không chấp nhận các trường hợp gian lận
+- Trong trường hợp xảy ra tranh chấp, mọi quyết định sẽ thuộc về Utop
+
+- Bạn sẽ thua nếu đụng trúng “Tên lửa” hoặc “Con dơi”.
+- Bạn sẽ thắng khi đạt được 600 điểm trong 1 phút.
+- Không chấp nhận các trường hợp gian lận
+- Trong trường hợp xảy ra tranh chấp, mọi quyết định sẽ thuộc về Utop`;
+
+
+/*- Tải app Utop và đăng kí tài khoản
+- Dùng camera trên app để quét mã QR trên màn hình TV
+- Nhấn nút “Bắt đầu” trên điện thoại để bắt đầu chơi
+
+<b>Cách chơi:</b>
+- Người chơi đứng vào vị trí đã định sẵn
+- Di chuyển người lên và xuống điều khiển nhân vật trên màn hình TV để thu thập thức ăn và tránh các chướng ngại vật
+
+<b>Thời gian:</b> từ ngày 15/9/2020 đến ngày 29/9/2020
+
+<b>Giải thưởng:</b>
+Coupon Utop 49,000VND để ăn thỏa thích tại Khu ăn
+uống FoodCourt - Tòa tháp Tài Chính Bitexco, Quận 1, TPHCM
+
+<b>Thể lệ trò chơi:</b>
+- Trò chơi có <b>1 cấp độ</b>, bạn sẽ thắng khi đạt được 600 trong 1 lượt chơi
+- Thu thập đồ ăn để nhận được điểm
+- Bạn sẽ thua nếu và trúng “Tên lửa” hoặc “Con dơi”. Vậy nên cẩn thận nhé!
+- Mỗi người có <b>3 lượt chơi</b>
+- Không chấp nhận các trường hợp gian lận
+- Trong trường hợp xảy ra tranh chấp, mọi quyết định sẽ thuộc về Utop*/
