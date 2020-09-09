18aee9e9b67d1eda0cdbc14432fa3be3464e7933
laminhtruong <laminhtruong@gmail.com>
2020-09-09 10:26:11 +0700

    - Increase player speed

--- test/parameters.3de2c.js
+++ test/parameters.3de2c.js
@@ -1,3 +1,3 @@
 var userParams = {};
 userParams.api = "https://utop-app-game-qa.azurewebsites.net/GamePlay";
-userParams.landing = "https://s-test.utop.vn/vanhanhmall?gc={gc}&ch={ch}&t={t}";
+userParams.landing = "https://s-test.utop.vn/vanhanhmall";
