b6d890205009ce413bcaa520b79fda0bd7ff57ed
laminhtruong <laminhtruong@gmail.com>
2020-09-10 14:54:35 +0700

    - Correct help

--- utop-landing-page/parameters.a3c22.js
+++ utop-landing-page/parameters.a3c22.js
@@ -16,7 +16,8 @@ userParams.helpVanHanhMall = `- Tải app Utop và đăng kí tài khoản
 <b>Thể lệ trò chơi:</b>
 - Trò chơi có <b>1 cấp độ</b>, bạn sẽ thắng khi đạt được <b>600</b> trong 1 lượt chơi
 - Thu thập đồ ăn để nhận được điểm
-- Bạn sẽ thua nếu và trúng “Tên lửa” hoặc “Con dơi”. Vậy nên cẩn thận nhé!
+- Bạn sẽ thua nếu va trúng “Tên lửa” hoặc “Con dơi”.
+Vậy nên cẩn thận nhé!
 - Mỗi người có <b>3 lượt chơi</b>
 - Không chấp nhận các trường hợp gian lận
 - Trong trường hợp xảy ra tranh chấp, mọi quyết định sẽ thuộc về Utop`
@@ -32,13 +33,15 @@ userParams.helpBitexco = `- Tải app Utop và đăng kí tài khoản
 <b>Thời gian:</b> từ ngày 15/9/2020 đến ngày 29/9/2020
 
 <b>Giải thưởng:</b>
-- Coupon Utop 39,000VND để ăn thỏa thích tại Khu ăn uống FoodCourt - Tòa tháp Tài Chính Bitexco, Quận 1, TPHCM
+- Coupon Utop 39,000VND để ăn thỏa thích tại Khu ăn
+uống FoodCourt - Tòa tháp Tài Chính Bitexco, Quận 1, TPHCM
 <i>* Mỗi tài khoản chỉ được nhận thưởng 1 lần</i>
 
 <b>Thể lệ trò chơi:</b>
 - Trò chơi có <b>1 cấp độ</b>, bạn sẽ thắng khi đạt được <b>600</b> trong 1 lượt chơi
 - Thu thập đồ ăn để nhận được điểm
-- Bạn sẽ thua nếu và trúng “Tên lửa” hoặc “Con dơi”. Vậy nên cẩn thận nhé!
+- Bạn sẽ thua nếu va trúng “Tên lửa” hoặc “Con dơi”.
+Vậy nên cẩn thận nhé!
 - Mỗi người có <b>3 lượt chơi</b>
 - Không chấp nhận các trường hợp gian lận
 - Trong trường hợp xảy ra tranh chấp, mọi quyết định sẽ thuộc về Utop`
