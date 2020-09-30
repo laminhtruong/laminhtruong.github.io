8c4aaef0edff5cdae63b7b66b62d99edc1dd0df2
laminhtruong <laminhtruong@gmail.com>
2020-09-30 15:40:42 +0700

    - Enable debug

---
+++ utop-mobile/src/settings.cd118.js
@@ -0,0 +1,25 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scenes/main.fire",
+    orientation: "",
+    debug: true,
+    jsList: [
+        "assets/scripts/libs/sha1.min.e140d.js"
+    ],
+    bundleVers: {
+        internal: "267e3",
+        main: "77f75"
+    }
+};
