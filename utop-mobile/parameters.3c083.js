7d3ec7d3bf4b18fe351fcc4a30f678c320ab9e22
laminhtruong <laminhtruong@gmail.com>
2020-10-11 17:33:00 +0700

    - Update

---
+++ utop-mobile/parameters.3c083.js
@@ -0,0 +1,38 @@
+var userParams = {};
+userParams.winScore = 500;
+userParams.helpPortrait = `- Nhấn nút “Chơi Ngay” trên điện thoại để
+bắt đầu chơi
+
+<b>Cách chơi:</b>
+- Di chuyển lên và xuống để điều khiển nhân vật trên màn hình điện thoại
+- Thu thập thức ăn và tránh các chướng ngại vật
+
+<b>Thời gian:</b>
+
+<b>Giải thưởng:</b>
+- Coupon Utop sử dụng tại cách thương hiệu đối tác của Utop
+
+<b>Thể lệ trò chơi:</b>
+- Trò chơi có 1 cấp độ, bạn sẽ thắng khi đạt được 500 điểm
+- Bạn sẽ thua nếu va trúng “Tên lửa” hoặc
+“Con dơi”
+- Không chấp nhận các trường hợp gian lận
+- Trong trường hợp xảy ra tranh chấp, mọi quyết định sẽ thuộc về Utop`
+
+userParams.helpLandscape = `- Nhấn nút “Chơi Ngay” trên điện thoại để bắt đầu chơi
+
+<b>Cách chơi:</b>
+- Di chuyển lên và xuống để điều khiển nhân vật trên màn hình điện thoại
+- Thu thập thức ăn và tránh các chướng ngại vật
+
+<b>Thời gian:</b>
+
+<b>Giải thưởng:</b>
+- Coupon Utop sử dụng tại cách thương hiệu đối tác của Utop
+
+<b>Thể lệ trò chơi:</b>
+- Trò chơi có 1 cấp độ, bạn sẽ thắng khi đạt được 500
+điểm
+- Bạn sẽ thua nếu va trúng “Tên lửa” hoặc “Con dơi”
+- Không chấp nhận các trường hợp gian lận
+- Trong trường hợp xảy ra tranh chấp, mọi quyết định sẽ thuộc về Utop`
