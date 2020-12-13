e48c761e82fbe3421b8d8f784a28bf9ba8834a66
laminhtruong <laminhtruong@gmail.com>
2020-12-13 22:24:16 +0700

    - Update

--- utop/parameters.b50a5.js
+++ utop/parameters.b50a5.js
@@ -1,3 +1,4 @@
 var userParams = {};
 userParams.api = "https://utop-app-game-qa.azurewebsites.net/GamePlay";
 userParams.landing = "https://s-test.utop.vn/vanhanhmall";
+userParams.useReward = false;
