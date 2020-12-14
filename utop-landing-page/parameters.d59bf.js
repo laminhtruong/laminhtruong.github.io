cf7245ca8da215b07d114e4921499dcc2d116905
laminhtruong <laminhtruong@gmail.com>
2020-12-14 12:50:48 +0700

    - Update

---
+++ utop-landing-page/parameters.d59bf.js
@@ -0,0 +1,17 @@
+var userParams = {};
+userParams.api = "https://utop-app-game-qa.azurewebsites.net/GamePlay";
+userParams.helpVanHanhMall = `Nhấn nút <b>"Chơi Ngay"</b> trên điện thoại để bắt đầu chơi.
+
+<b>Cách chơi:</b>
+- Người chơi đứng vào vị trí đã định sẵn.
+
+- Di chuyển người lên và xuống điều
+khiển nhân vật trên màn hình TV để thu thập thức ăn và tránh các chướng ngại vật.`
+
+userParams.helpBitexco = `Nhấn nút <b>"Chơi Ngay"</b> trên điện thoại để bắt đầu chơi.
+
+<b>Cách chơi:</b>
+- Người chơi đứng vào vị trí đã định sẵn.
+
+- Di chuyển người lên và xuống điều
+khiển nhân vật trên màn hình TV để thu thập thức ăn và tránh các chướng ngại vật.`
---
+++ utop/src/settings.20c1b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"",jsList:["assets/scripts/libs/sha1.min.29ba3.js"],bundleVers:{internal:"620a7",main:"7bdc8"}};
