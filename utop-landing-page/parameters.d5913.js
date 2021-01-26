ad4365c03b23714b7c354f7a539e1a9f9c503731
laminhtruong <laminhtruong@gmail.com>
2021-01-26 12:17:59 +0700

    - Update

---
+++ utop-landing-page/parameters.d5913.js
@@ -0,0 +1,39 @@
+var userParams = {};
+userParams.api = "https://utop-app-game-qa.azurewebsites.net/GamePlay";
+userParams.helpVanHanhMall = `<b>Tải app Utop</b> và <b>đăng kí tài khoản.</b>
+Dùng camera trên app để <b>quét mã QR trên màn hình TV.</b>
+Nhấn nút <b>"Chơi ngay"</b> trên điện thoại để chơi.
+
+<b><color=#7d1012>CÁCH CHƠI</color></b>
+- Người chơi đứng vào vị trí đã định sẵn.
+- Di chuyển người qua trái và phải điều khiển nhân vật trên màn hình TV để thu thập <b>Đồ Ăn</b> và tránh <b>Pháo</b>.
+
+<b><color=#7d1012>GIẢI THƯỞNG</color></b>
+- Voucher Utop giảm giá 50.000 đồng cho đơn hàng từ 100.000 đồng, ăn thoả thích tại khu ăn uống Food Terrace,
+tầng 2,  Aeon Mall Tân Phú Celadon.
+- Bao gồm: Katong, Tealive, Món Hàn, Món Việt, Haisancua.com, Bangkok House, Cơm Ngon Lẩu Công Chúa, Hoàng Gia, ChiChiKo, Hoài Bão, Papaxot, Việt Deli, Lotteria, Bobapop,
+BJ Tokbokki.
+- Mỗi tài khoản Utop chỉ được nhận giải thưởng 01 lần.
+
+<b><color=#7d1012>THỂ LỆ TRÒ CHƠI</color></b>
+- Trò chơi có 1 cấp độ, bạn sẽ thắng khi đạt được 300 điểm trong 1 lượt chơi với thời gian tối đa là 45 giây
+- Thời gian: từ ngày 28/01/2021 đến ngày 08/02/2021`
+
+userParams.helpBitexco = `<b>Tải app Utop</b> và <b>đăng kí tài khoản.</b>
+Dùng camera trên app để <b>quét mã QR trên màn hình TV.</b>
+Nhấn nút <b>"Chơi ngay"</b> trên điện thoại để chơi.
+
+<b><color=#7d1012>CÁCH CHƠI</color></b>
+- Người chơi đứng vào vị trí đã định sẵn.
+- Di chuyển người qua trái và phải điều khiển nhân vật trên màn hình TV để thu thập <b>Đồ Ăn</b> và tránh <b>Pháo</b>.
+
+<b><color=#7d1012>GIẢI THƯỞNG</color></b>
+- Voucher Utop giảm giá 50.000 đồng cho đơn hàng từ 100.000 đồng, ăn thoả thích tại khu ăn uống Food Terrace,
+tầng 2,  Aeon Mall Tân Phú Celadon.
+- Bao gồm: Katong, Tealive, Món Hàn, Món Việt, Haisancua.com, Bangkok House, Cơm Ngon Lẩu Công Chúa, Hoàng Gia, ChiChiKo, Hoài Bão, Papaxot, Việt Deli, Lotteria, Bobapop,
+BJ Tokbokki.
+- Mỗi tài khoản Utop chỉ được nhận giải thưởng 01 lần.
+
+<b><color=#7d1012>THỂ LỆ TRÒ CHƠI</color></b>
+- Trò chơi có 1 cấp độ, bạn sẽ thắng khi đạt được 300 điểm trong 1 lượt chơi với thời gian tối đa là 45 giây
+- Thời gian: từ ngày 28/01/2021 đến ngày 08/02/2021`
