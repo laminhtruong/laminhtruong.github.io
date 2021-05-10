9aa9b6d382e301f50d16207a30ee49ad9f2a0aef
laminhtruong <laminhtruong@gmail.com>
2021-05-10 22:10:01 +0700

    - Update

--- mytour-auction/index.b000a.js
+++ mytour-auction/index.b000a.js
@@ -1,4 +1,4 @@
-System.register(["./application.f6d8b.js"], function (_export, _context) {
+System.register(["./application.44196.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.f6d8b.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationF6d8bJs) {
-      createApplication = _applicationF6d8bJs.createApplication;
+    setters: [function (_application44196Js) {
+      createApplication = _application44196Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
