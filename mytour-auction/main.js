3cc7714e79bdd504d85498123b4303d02c87980a
laminhtruong <laminhtruong@gmail.com>
2021-05-15 20:10:54 +0700

    - Update

--- mytour-auction/main.js
+++ mytour-auction/main.js
@@ -1,8 +1,17 @@
 function Alignment()
 {
     var iframe = document.getElementById("game");
+    var param = window.parent.location.href.split('?')[1];
+    if (param)
+    {
+        param = `?${param}`;
+    }
+    else
+    {
+        param = "";
+    }
     iframe.style.height = window.innerHeight;
     iframe.style.width = window.innerHeight * 9 / 16;
     iframe.style.display = "block"
-    iframe.setAttribute("src", "./game/index.html");
+    iframe.setAttribute("src", `./game/index.html${param}`);
 }
