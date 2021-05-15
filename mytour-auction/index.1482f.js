bb47f2d241cbebfb3254fc8742d361579ccdaf0b
laminhtruong <laminhtruong@gmail.com>
2021-05-15 19:45:19 +0700

    - Bug fixed

--- mytour-auction/index.1482f.js
+++ mytour-auction/index.1482f.js
@@ -1,4 +1,4 @@
-System.register(["./application.8f861.js"], function (_export, _context) {
+System.register(["./application.8d642.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.8f861.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application8f861Js) {
-      createApplication = _application8f861Js.createApplication;
+    setters: [function (_application8d642Js) {
+      createApplication = _application8d642Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
