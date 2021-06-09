257ea5448beaf97cce8a83cf715e0938209db5d1
laminhtruong <laminhtruong@gmail.com>
2021-06-09 16:19:42 +0700

    - Update

--- arjs/application.81cc7.js
+++ arjs/application.81cc7.js
@@ -94,7 +94,7 @@ System.register([], function (_export, _context) {
 
     function loadSettingsJson(cc) {
       var server = '';
-      var settings = 'src/settings.244c0.json';
+      var settings = 'src/settings.ede0d.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
--- arjs/index.ebe98.js
+++ arjs/index.ebe98.js
@@ -1,4 +1,4 @@
-System.register(["./application.10867.js"], function (_export, _context) {
+System.register(["./application.81cc7.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.10867.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application10867Js) {
-      createApplication = _application10867Js.createApplication;
+    setters: [function (_application81cc7Js) {
+      createApplication = _application81cc7Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
