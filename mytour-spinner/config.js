1f604f054e46df201f519cf43108c1fc10ece6b2
laminhtruong <laminhtruong@gmail.com>
2021-07-07 20:31:34 +0700

    - spinner

---
+++ mytour-spinner/config.js
@@ -0,0 +1,8 @@
+window.API_URL = 'https://gate.dev.tripi.vn/tripi-feed';
+window.API_HASH = 'AsnasjldknsajalLASASs86876n4oadn5aod3nlNSLA';
+window.SHARE_URL = 'https://feed-dev.mytour.vn'
+window.APPLE_STORE_URL = 'https://mytour.onelink.me/4281031074/1ec8c4c7?af_qr=true';
+window.GOOGLE_STORE_URL = 'https://mytour.onelink.me/4281031074/1ec8c4c7?af_qr=true';
+window.DOWNLOAD_MULTIPLY = 3;
+window.TURN_REFRESH_TIME = 300;
+window.LOADING_TIME = 4000;
