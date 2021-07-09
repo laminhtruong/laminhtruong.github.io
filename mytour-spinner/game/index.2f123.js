99b297521d65694d212d14911e2b56e402a8e99e
laminhtruong <laminhtruong@gmail.com>
2021-07-09 13:35:40 +0700

    - Update

--- mytour-spinner/game/index.2f123.js
+++ mytour-spinner/game/index.2f123.js
@@ -1,4 +1,4 @@
-System.register(["./application.b1d88.js"], function (_export, _context) {
+System.register(["./application.6dc47.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.b1d88.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationB1d88Js) {
-      createApplication = _applicationB1d88Js.createApplication;
+    setters: [function (_application6dc47Js) {
+      createApplication = _application6dc47Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
