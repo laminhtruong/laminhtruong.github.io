aad15f0d5c45617108ed4b11d58bc65afed1da13
laminhtruong <laminhtruong@gmail.com>
2021-07-13 09:16:38 +0700

    - Update

--- mytour-spinner/game/index.f025a.js
+++ mytour-spinner/game/index.f025a.js
@@ -1,4 +1,4 @@
-System.register(["./application.3211a.js"], function (_export, _context) {
+System.register(["./application.9494f.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.3211a.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application3211aJs) {
-      createApplication = _application3211aJs.createApplication;
+    setters: [function (_application9494fJs) {
+      createApplication = _application9494fJs.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
