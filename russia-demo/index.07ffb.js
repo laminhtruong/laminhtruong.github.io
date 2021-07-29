b36407a5ff3208be16a436345c548af2d563cfa4
laminhtruong <laminhtruong@gmail.com>
2021-07-29 12:12:40 +0700

    - Update

--- russia-demo/index.07ffb.js
+++ russia-demo/index.07ffb.js
@@ -1,4 +1,4 @@
-System.register(["./application.f8cd8.js"], function (_export, _context) {
+System.register(["./application.9a4fb.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.f8cd8.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationF8cd8Js) {
-      createApplication = _applicationF8cd8Js.createApplication;
+    setters: [function (_application9a4fbJs) {
+      createApplication = _application9a4fbJs.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
