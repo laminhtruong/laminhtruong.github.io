b730bc3986c5a95c4f54a1d8ed62e9ecc2cf9922
laminhtruong <laminhtruong@gmail.com>
2021-11-06 14:43:33 +0700

    - Alignment

--- crabada-running/index.31d77.js
+++ crabada-running/index.31d77.js
@@ -1,4 +1,4 @@
-System.register(["./application.71d93.js"], function (_export, _context) {
+System.register(["./application.a088b.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.71d93.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application71d93Js) {
-      createApplication = _application71d93Js.createApplication;
+    setters: [function (_applicationA088bJs) {
+      createApplication = _applicationA088bJs.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
