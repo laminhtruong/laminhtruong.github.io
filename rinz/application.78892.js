ecc2d781dd7d7b00da38216a9146f2f7e7b7d737
laminhtruong <laminhtruong@gmail.com>
2021-11-28 11:36:42 +0700

    - Updat

--- rinz/application.78892.js
+++ rinz/application.78892.js
@@ -96,7 +96,7 @@ System.register([], function (_export, _context) {
 
     function loadSettingsJson(cc) {
       var server = '';
-      var settings = 'src/settings.83f0d.json';
+      var settings = 'src/settings.9c7d7.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
--- rinz/index.3cfe0.js
+++ rinz/index.3cfe0.js
@@ -1,4 +1,4 @@
-System.register(["./application.a088b.js"], function (_export, _context) {
+System.register(["./application.78892.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.a088b.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationA088bJs) {
-      createApplication = _applicationA088bJs.createApplication;
+    setters: [function (_application78892Js) {
+      createApplication = _application78892Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
