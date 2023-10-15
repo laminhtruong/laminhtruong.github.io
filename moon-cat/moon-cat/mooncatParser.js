58e550dd9f6cbb0cd6791d9364c36ffcfd096710
laminhtruong <laminhtruong@gmail.com>
2023-10-15 21:01:55 +0700

    - Update

--- moon-cat/moon-cat/mooncatParser.js
+++ moon-cat/moon-cat/mooncatParser.js
@@ -484,10 +484,10 @@ export const MoonCatParser = (function ()
 			for (let i = 0; i < 8; i++)
 			{
 				let spriteRow = indecies[i];
-				if (k < 64)
-				{
-					spriteRow = indeciesMirrored[i];
-				}
+				// if (k < 64)
+				// {
+				// 	spriteRow = indeciesMirrored[i];
+				// }
 				if (i == 6)
 				{
 					// Handle the facing forward pose animations
@@ -527,14 +527,14 @@ export const MoonCatParser = (function ()
 
 					if (spriteRow == 2)
 					{
-						if (k < 64)
-						{
-							if (j == 3 || j == 4)
-							{
-								offsetX = -1;
-							}
-						}
-						else
+						// if (k < 64)
+						// {
+						// 	if (j == 3 || j == 4)
+						// 	{
+						// 		offsetX = -1;
+						// 	}
+						// }
+						// else
 						{
 							if (j == 2 || j == 5)
 							{
@@ -572,10 +572,10 @@ export const MoonCatParser = (function ()
 					if (pixelMap)
 					{
 						// console.log('FOUND: ', key1);
-						if (k < 64)
-						{
-							pixelMap = mirror(pixelMap);
-						}
+						// if (k < 64)
+						// {
+						// 	pixelMap = mirror(pixelMap);
+						// }
 						// if(k >= 64 && (spriteRow == 0 || spriteRow == 8)){
 						//   pixelMap = mirror(pixelMap);
 						// }
