2a491ccc023f336fd68b7b3011854c17bc007221
laminhtruong <laminhtruong@gmail.com>
2024-02-27 13:36:04 +0700

    - Update

--- x-sushi/application.faf07.js
+++ x-sushi/application.faf07.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.json';
+          this.settingsPath = 'src/settings.9e74a.json';
           this.showFPS = false;
         }
 
--- x-sushi/index.200a7.js
+++ x-sushi/index.200a7.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.faf07.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationJs) {
-      Application = _applicationJs.Application;
+    setters: [function (_applicationFaf07Js) {
+      Application = _applicationFaf07Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
