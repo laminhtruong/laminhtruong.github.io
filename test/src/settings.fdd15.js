a3e0a68e2bb648542019b7667ed5baaf5f9f8007
laminhtruong <laminhtruong@gmail.com>
2024-07-04 21:31:43 +0700

    - Delete old project

--- test/src/settings.fdd15.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"",jsList:["assets/scripts/libs/sha1.min.29ba3.js"],bundleVers:{internal:"f1413",main:"bb875"}};
