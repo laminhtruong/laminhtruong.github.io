0181a2096cc04bb8a14dbaebee9eb6912bec06e1
laminhtruong <laminhtruong@gmail.com>
2024-07-07 19:44:40 +0700

    - Bug fixed

--- merge-pals-dev-7/script.js
+++ merge-pals-dev-7/script.js
@@ -48,11 +48,33 @@ script.onload = function () {
     }).then(function (unityInstance) {
         unityGame = unityInstance;
         loadingContainer.classList.add("finished");
+        disableScrollDown();
     }).catch(function (message) {
         alert(message);
     });
 };
 document.body.appendChild(script);
+
+function disableScrollDown() {
+    const overflow = 100;
+    document.body.style.overflowY = 'hidden';
+    document.body.style.marginTop = `${overflow}px`;
+    document.body.style.height = window.innerHeight + overflow + "px";
+    document.body.style.paddingBottom = `${overflow}px`;
+    window.scrollTo(0, overflow);
+
+    let ts = 0;
+    const onTouchStart = (e) => {
+        ts = e.touches[0].clientY;
+    };
+    const onTouchMove = (e) => {
+        e.preventDefault();
+    };
+
+    document.documentElement.addEventListener('touchstart', onTouchStart, { passive: false });
+    document.documentElement.addEventListener('touchmove', onTouchMove, { passive: false });
+}
+
 function runUnityCommand(method, params) {
     unityGame?.SendMessage("WebBridge", method, params);
 }
