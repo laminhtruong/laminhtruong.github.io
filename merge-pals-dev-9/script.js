5b0ae9b4614dd0128a0eb3db8e8f192c3b5d09da
laminhtruong <laminhtruong@gmail.com>
2024-07-15 21:31:02 +0700

    - Update

--- merge-pals-dev-9/script.js
+++ merge-pals-dev-9/script.js
@@ -1,82 +1,98 @@
+window.addEventListener("load", function () {
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker.register("ServiceWorker.js");
+	}
+});
+
+var container = document.querySelector("#unity-container");
+var canvas = document.querySelector("#unity-canvas");
+var loadingBar = document.querySelector("#unity-loading-bar");
+var progressBarFull = document.querySelector("#unity-progress-bar-full");
+var warningBanner = document.querySelector("#unity-warning");
+
 var buildUrl = "Build";
-var loaderUrl = buildUrl + "/development.loader.js?638562238391208560";
+var loaderUrl = buildUrl + "/development.loader.js?638566745758672351";
 var config = {
-    dataUrl: buildUrl + "/4bb3114a5e833ab25fb6530a77e23be9.data.unityweb?638562238391208560",
-    frameworkUrl: buildUrl + "/f5b270c4535b2ba0715abd8948657e80.js.unityweb?638562238391208560",
-    codeUrl: buildUrl + "/121e02dad53c360dd196865be919331a.wasm.unityweb?638562238391208560",
-    streamingAssetsUrl: "StreamingAssets",
-    companyName: "Mirailabs",
-    productName: "Merge Pals",
-    productVersion: "1.0.0",
+	dataUrl: buildUrl + "/85cb22cb7902da1a1828d05c475c1bc3.data.unityweb?638566745758672351",
+	frameworkUrl: buildUrl + "/4ba2751d13eece24e5c0cb1772c25a49.js.unityweb?638566745758672351",
+	codeUrl: buildUrl + "/d81d299511578ad017e0620b1de711d1.wasm.unityweb?638566745758672351",
+	streamingAssetsUrl: "StreamingAssets",
+	companyName: "Mirailabs",
+	productName: "Merge Pals",
+	productVersion: "1.0.0",
 
-    cacheControl: function (url) {
-        // Caching enabled for .data and .bundle files.
-        // Revalidate if file is up to date before loading from cache
-        if (url.match(/\.data/) || url.match(/\.bundle/)) {
-            return "must-revalidate";
-        }
+	cacheControl: function (url) {
+		// Caching enabled for .data and .bundle files.
+		// Revalidate if file is up to date before loading from cache
+		if (url.match(/\.data/) || url.match(/\.bundle/)) {
+			return "must-revalidate";
+		}
 
-        // Caching enabled for .mp4 and .custom files
-        // Load file from cache without revalidation.
-        if (url.match(/\.mp4/) || url.match(/\.custom/)) {
-            return "immutable";
-        }
+		// Caching enabled for .mp4 and .custom files
+		// Load file from cache without revalidation.
+		if (url.match(/\.mp4/) || url.match(/\.custom/)) {
+			return "immutable";
+		}
 
-        // Disable explicit caching for all other files.
-        // Note: the default browser cache may cache them anyway.
-        return "no-store";
-    },
+		// Disable explicit caching for all other files.
+		// Note: the default browser cache may cache them anyway.
+		return "no-store";
+	},
 };
 
-var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-if(isMobile) {
-    // Define a maximum pixel ratio for mobile to avoid rendering at too high resolutions
-    const maxPixelRatioMobile = 2.0;
-    config.devicePixelRatio = Math.min(window.devicePixelRatio, maxPixelRatioMobile);
-}
+// By default Unity keeps WebGL canvas render target size matched with
+// the DOM size of the canvas element (scaled by window.devicePixelRatio)
+// Set this to false if you want to decouple this synchronization from
+// happening inside the engine, and you would instead like to size up
+// the canvas DOM size and WebGL render target sizes yourself.
+// config.matchWebGLToCanvasSize = false;
 
-var canvas = document.querySelector("#unity-canvas");
-var loadingContainer = document.querySelector("#unity-loading-container");
-var loadingBar = document.querySelector("#unity-loading-bar-inner");
+if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+	// Mobile device style: fill the whole browser client area with the game canvas:
+	var meta = document.createElement('meta');
+	meta.name = 'viewport';
+	meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+	document.getElementsByTagName('head')[0].appendChild(meta);
+}
 
 var unityGame;
 var script = document.createElement("script");
 script.src = loaderUrl;
-script.onload = function() {
-    createUnityInstance(canvas, config, function(progress) {
-        loadingBar.style.width = 100 * progress + "%";
-    }).then(function(unityInstance) {
-        unityGame = unityInstance;
-        loadingContainer.classList.add("finished");
-    }).catch(function(message) {
-        alert(message);
-    });
+script.onload = () => {
+	createUnityInstance(canvas, config, (progress) => {
+		progressBarFull.style.width = 100 * progress + "%";
+	}).then((unityInstance) => {
+		unityGame = unityInstance;
+		loadingContainer.classList.add("finished");
+	}).catch((message) => {
+		alert(message);
+	});
 };
 document.body.appendChild(script);
-function runUnityCommand(method,params){
-    unityGame?.SendMessage("WebBridge",method,params);
-}
-function UnityTaskCallBack(taskId,success,data){
-    runUnityCommand("UnityTaskCallBack",JSON.stringify({
-        taskId,
-        success,
-        data: ((typeof data === 'object' && data !== null)?JSON.stringify(data):data.toString())
-    }));
+
+function runUnityCommand(method, params) {
+	unityGame?.SendMessage("WebBridge", method, params);
 }
 
-const tonWeb=new TonWeb(new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC'));
+function UnityTaskCallBack(taskId, success, data) {
+	runUnityCommand("UnityTaskCallBack", JSON.stringify({
+		taskId,
+		success,
+		data: ((typeof data === 'object' && data !== null) ? JSON.stringify(data) : data.toString())
+	}));
+}
 
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-    manifestUrl: 'https://mirai-labs.sgp1.cdn.digitaloceanspaces.com/merge_pal/static/1254741318562603008.json'
+	manifestUrl: 'https://mirai-labs.sgp1.cdn.digitaloceanspaces.com/merge_pal/static/1254741318562603008.json'
 });
 
 tonConnectUI.onStatusChange(walletAndwalletInfo => {
-    runUnityCommand("StatusChange");
+	runUnityCommand("StatusChange");
 });
 tonConnectUI.connectionRestored.then(restored => {
-    if (restored) {
-        console.log('Connection restored.');
-    } else {
-        console.log('Connection was not restored.');
-    }
+	if (restored) {
+		console.log('Connection restored.');
+	} else {
+		console.log('Connection was not restored.');
+	}
 });
