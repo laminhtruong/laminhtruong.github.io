80d17d21609b1f56fc539965e6848f70019d77cc
La Minh Truong <truongla@pegaxy.com>
2024-07-17 11:43:44 +0700

    - merge pal mirai

---
+++ merge-pals-mirai/script.js
@@ -0,0 +1,83 @@
+var buildUrl = "Build";
+var loaderUrl = buildUrl + "/development.loader.js?638568130257862999";
+var config = {
+    dataUrl: buildUrl + "/743dd024b8a4abf1fa4d72cdd128cfcd.data.unityweb?638568130257862999",
+    frameworkUrl: buildUrl + "/69ebfd1562918b109be08c93f3b6790a.js.unityweb?638568130257862999",
+    codeUrl: buildUrl + "/d79bd12827fc75d002c47f80d72dec08.wasm.unityweb?638568130257862999",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "Mirailabs",
+    productName: "Merge Pals",
+    productVersion: "1.0.0",
+
+    cacheControl: function (url) {
+        // Caching enabled for .data and .bundle files.
+        // Revalidate if file is up to date before loading from cache
+        if (url.match(/\.data/) || url.match(/\.bundle/)) {
+            return "must-revalidate";
+        }
+
+        // Caching enabled for .mp4 and .custom files
+        // Load file from cache without revalidation.
+        if (url.match(/\.mp4/) || url.match(/\.custom/)) {
+            return "immutable";
+        }
+
+        // Disable explicit caching for all other files.
+        // Note: the default browser cache may cache them anyway.
+        return "no-store";
+    },
+};
+
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+    // Define a maximum pixel ratio for mobile to avoid rendering at too high resolutions
+    const maxPixelRatioMobile = 2.0;
+    config.devicePixelRatio = Math.min(window.devicePixelRatio, maxPixelRatioMobile);
+}
+else {
+    config.devicePixelRatio = 2.0;
+}
+
+var canvas = document.querySelector("#unity-canvas");
+var loadingContainer = document.querySelector("#unity-loading-container");
+var loadingBar = document.querySelector("#unity-loading-bar-inner");
+
+var unityGame;
+var script = document.createElement("script");
+script.src = loaderUrl;
+script.onload = function () {
+    createUnityInstance(canvas, config, function (progress) {
+        loadingBar.style.width = 100 * progress + "%";
+    }).then(function (unityInstance) {
+        unityGame = unityInstance;
+        loadingContainer.classList.add("finished");
+    }).catch(function (message) {
+        alert(message);
+    });
+};
+document.body.appendChild(script);
+function runUnityCommand(method, params) {
+    unityGame?.SendMessage("WebBridge", method, params);
+}
+function UnityTaskCallBack(taskId, success, data) {
+    runUnityCommand("UnityTaskCallBack", JSON.stringify({
+        taskId,
+        success,
+        data: ((typeof data === 'object' && data !== null) ? JSON.stringify(data) : data.toString())
+    }));
+}
+
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+    manifestUrl: 'https://mirai-labs.sgp1.cdn.digitaloceanspaces.com/merge_pal/static/1254741318562603008.json'
+});
+
+tonConnectUI.onStatusChange(walletAndwalletInfo => {
+    runUnityCommand("StatusChange");
+});
+tonConnectUI.connectionRestored.then(restored => {
+    if (restored) {
+        console.log('Connection restored.');
+    } else {
+        console.log('Connection was not restored.');
+    }
+});
