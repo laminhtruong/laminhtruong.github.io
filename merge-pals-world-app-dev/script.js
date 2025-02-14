fb0870230c5e52b5155abd53b0d59f15fc1cf577
La Minh Truong <truongla@pegaxy.com>
2025-02-14 10:10:01 +0700

    - world app dev

---
+++ merge-pals-world-app-dev/script.js
@@ -0,0 +1,57 @@
+var buildUrl = "Build";
+var loaderUrl = buildUrl + "/development.loader.js?638751234642696388";
+var config = {
+    dataUrl: buildUrl + "/e581d76c5f7cc97667beeb02f2b1fc14.data.unityweb",
+    frameworkUrl: buildUrl + "/5e9e75b1af5e6099fc7004a77b86f193.js.unityweb",
+    codeUrl: buildUrl + "/1773ffc154fffcb6ed91d04181f501cb.wasm.unityweb",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "Mirailabs",
+    productName: "Merge Pals",
+    productVersion: "2.0.0",
+
+    cacheControl: function (url) {
+        // Caching enabled for .data and .bundle files.
+        // Revalidate if file is up to date before loading from cache
+        if (url.match(/\.data/) || url.match(/\.bundle/)) {
+            return "must-revalidate";
+        }
+
+        // Caching enabled for .mp4 and .custom files
+        // Load file from cache without revalidation.
+        if (url.match(/\.mp4/) || url.match(/\.custom/)) {
+            return "immutable";
+        }
+
+        // Disable explicit caching for all other files.
+        // Note: the default browser cache may cache them anyway.
+        return "no-store";
+    },
+};
+
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+    // Define a maximum pixel ratio for mobile to avoid rendering at too high resolutions
+    const maxPixelRatioMobile = 2.0;
+    config.devicePixelRatio = Math.min(window.devicePixelRatio, maxPixelRatioMobile);
+}
+else {
+    config.devicePixelRatio = 2.0;
+}
+
+var canvas = document.querySelector("#unity-canvas");
+var loadingBar = document.querySelector("#unity-loading-fg");
+
+var unityGame;
+var script = document.createElement("script");
+script.src = loaderUrl;
+script.onload = function () {
+    createUnityInstance(canvas, config, function (progress) {
+        loadingBar.style.width = 75 * progress + "%";
+    }).then(function (unityInstance) {
+        unityGame = unityInstance;
+    }).catch(function (message) {
+        alert(message);
+    });
+};
+document.body.appendChild(script);
+
