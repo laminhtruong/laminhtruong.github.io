54f36e81fa4bc225d7df327caec186c67afc4e44
La Minh Truong <truongla@pegaxy.com>
2025-05-28 10:30:14 +0700

    - Update

--- merge-pals-p/script.js
+++ merge-pals-p/script.js
@@ -1,10 +1,10 @@
-var buildUrl = "https://cdn.mirailabs.co/games/mergepals.io/web-build/Build";
+var buildUrl = "Build";
 var loaderUrl = buildUrl + "/production.loader.js?638840226438882636";
 var config = {
     dataUrl: buildUrl + "/77d68ccda526eb5944a31a0f6a377c74.data.unityweb",
     frameworkUrl: buildUrl + "/06b80fd24f230ed99aec6544f5aa9ef6.js.unityweb",
     codeUrl: buildUrl + "/cb9d1663b8206aa8c6d7690ff9006c83.wasm.unityweb",
-    streamingAssetsUrl: "https://cdn.mirailabs.co/games/mergepals.io/web-build/StreamingAssets",
+    streamingAssetsUrl: "StreamingAssets",
     companyName: "Mirailabs",
     productName: "Merge Pals",
     productVersion: "2.2.0",
