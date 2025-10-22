30a9f44556c9892a4d9e686f7be3487ef0b7d2bc
laminhtruong <laminhtruong@gmail.com>
2025-10-22 11:42:23 +0700

    - sss

---
+++ sss-reborn/JsBridge/modules/bridgeModule.js
@@ -0,0 +1,111 @@
+class BridgeModule {
+	init() {
+		document.addEventListener("visibilitychange", function () {
+			unityGame.SendMessage("UnityBridge", "OnVisibilityChange", document.visibilityState);
+		});
+	}
+
+	getData(data) {
+		return (typeof data === 'object' && data !== null) ? JSON.stringify(data) : typeof data !== 'undefined' ? data.toString() : '';
+	}
+
+	getError(message) {
+		return "{\"message\": \"" + message + "\"}";
+	}
+
+	sendTaskCallback(taskId, success, data) {
+		let params = {
+			id: taskId,
+			success: success,
+			data: this.getData(data)
+		};
+		unityGame.SendMessage("UnityBridge", "ExecuteCallback", JSON.stringify(params));
+	}
+
+	setLoadingProgress(args) {
+		let progress = args.progress;
+		let loadingBar = document.querySelector("#unity-loading-fg");
+		loadingBar.style.width = 100 * args.progress + "%";
+
+		if (progress == 1) {
+			let loadingContainer = document.querySelector("#unity-loading-container");
+			loadingContainer.classList.add("finished");
+		}
+	}
+
+	setLoadingText(args) {
+		let loadingText = document.querySelector("#unity-loading-text");
+		loadingText.innerHTML = args.text;
+	}
+
+	copyToClipboard(args) {
+		ClipboardJS.copy(args.text);
+	}
+
+	reload(args) {
+		if (args.root) {
+			window.location.href = this.getRootLink();
+		} else {
+			location.reload();
+		}
+	}
+
+	isProduction() {
+		let host = window.location.host;
+		let isProduction = false;
+		return this.getData(isProduction);
+	}
+
+	getRootLink() {
+		let link = window.location.href.replace(window.location.search, '').replace(window.location.hash, '');
+		return this.getData(link);
+	}
+
+	getPlatform() {
+		let platform = 'unknown';
+
+		if (Telegram.WebApp.initData !== '') platform = "telegram";
+		else if (liff.isInClient()) platform = "line";
+		else platform = "web";
+
+		return this.getData(platform);
+	}
+
+	getParams() {
+		let query = window.location.search.substring(1);
+		let params = query.split("&");
+		let result = {};
+
+		for (let i = 0; i < params.length; i++) {
+			let pair = params[i].split("=");
+			if (pair[0] !== "") {
+				result[pair[0]] = pair[1];
+			}
+		}
+
+		query = window.location.hash.substring(1);
+		params = query.split("&");
+		for (let i = 0; i < params.length; i++) {
+			let pair = params[i].split("=");
+			if (pair[0] !== "") {
+				result[pair[0]] = pair[1];
+			}
+		}
+
+		return this.getData(result);
+	}
+
+	storageSet(args) {
+		localStorage.setItem(args.key, args.value);
+	}
+
+	storageGet(args) {
+		let value = localStorage.getItem(args.key);
+		if (value === null) value = "";
+
+		return this.getData(value);
+	}
+
+}
+
+export default new BridgeModule;
---
+++ sss-reborn/script.js
@@ -0,0 +1,61 @@
+var buildUrl = "Build";
+var loaderUrl = buildUrl + "/43e2502d740906e412ee4ebb331ab97f.loader.js?638967292123691084";
+var config = {
+    dataUrl: buildUrl + "/3cb1fae3f84df08b6222c15b39e14187.data.unityweb",
+    frameworkUrl: buildUrl + "/bb1936e648d803aa441b9d89d19e5e8b.framework.js.unityweb",
+    codeUrl: buildUrl + "/dde39dffd15ee7b41fee185555499c12.wasm.unityweb",
+    symbolsUrl: buildUrl + "/86756524aae85d8d259547a8ad2bbd2d.symbols.json.unityweb",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "sg-byte",
+    productName: "Super Sushi Samurai",
+    productVersion: "1.0.0",
+
+    cacheControl: function (url) {
+        // Caching enabled for .data and .bundle files.
+        // Revalidate if file is up to date before loading from cache
+        if (url.match(/\.data/)) {
+            return "must-revalidate";
+        }
+
+        if (url.match(/\.bundle/)) {
+            return "immutable";
+        }
+
+        // Caching enabled for .mp4 and .custom files
+        // Load file from cache without revalidation.
+        if (url.match(/\.mp4/) || url.match(/\.custom/)) {
+            return "immutable";
+        }
+
+        // Disable explicit caching for all other files.
+        // Note: the default browser cache may cache them anyway.
+        return "no-store";
+    },
+};
+
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+    // Define a maximum pixel ratio for mobile to avoid rendering at too high resolutions
+    const maxPixelRatioMobile = 2.0;
+    config.devicePixelRatio = Math.min(window.devicePixelRatio, maxPixelRatioMobile);
+}
+else {
+    config.devicePixelRatio = 2.0;
+}
+
+var canvas = document.querySelector("#unity-canvas");
+var loadingBar = document.querySelector("#unity-loading-fg");
+
+var unityGame;
+var script = document.createElement("script");
+script.src = loaderUrl;
+script.onload = function () {
+    createUnityInstance(canvas, config, function (progress) {
+        loadingBar.style.width = 75 * progress + "%";
+    }).then(function (unityInstance) {
+        unityGame = unityInstance;
+    }).catch(function (message) {
+        alert(message);
+    });
+};
+document.body.appendChild(script);
