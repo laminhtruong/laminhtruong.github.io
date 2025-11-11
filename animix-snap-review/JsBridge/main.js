52faf59f13145830e37f3e1f8dd8ecd59c691f61
laminhtruong <laminhtruong@gmail.com>
2025-11-11 11:49:36 +0700

    - Review version

---
+++ animix-snap-review/JsBridge/main.js
@@ -0,0 +1,22 @@
+import bridgeModule from './modules/bridgeModule.js?638978415292076001';
+import telegramModule from './modules/telegramModule.js?638978415292076001';
+import lineModule from './modules/lineModule.js?638978415292076001';
+import googleModule from './modules/googleModule.js?638978415292076001';
+
+window.bridgeModule = bridgeModule;
+window.telegramModule = telegramModule;
+window.lineModule = lineModule;
+window.googleModule = googleModule;
+
+import rainbowKitModule from './modules/rainbowKitModule.js?638978415292076001';
+window.rainbowKitModule = rainbowKitModule;
+
+window.executeFunctionByName = function (functionName, context /*, args */) {
+	var args = Array.prototype.slice.call(arguments, 2);
+	var namespaces = functionName.split(".");
+	var func = namespaces.pop();
+	for (var i = 0; i < namespaces.length; i++) {
+		context = context[namespaces[i]];
+	}
+	return context[func].apply(context, args);
+};
---
+++ animix-snap-review/script.js
@@ -0,0 +1,61 @@
+var buildUrl = "Build";
+var loaderUrl = buildUrl + "/69b0e0207a7dd6505101a3e8266a8789.loader.js?638978415292076001";
+var config = {
+    dataUrl: buildUrl + "/2093bf8fe8e8bee9536f31722da573f7.data.unityweb",
+    frameworkUrl: buildUrl + "/b5c7eaf4f0d061310ccb715b44161cf7.framework.js.unityweb",
+    codeUrl: buildUrl + "/8060da8cdd5d5a96a7483becd2edc137.wasm.unityweb",
+    symbolsUrl: buildUrl + "/2851b251acbb831fc135658c559d6fc8.symbols.json.unityweb",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "sg-byte",
+    productName: "Animix Snap",
+    productVersion: "1.0.4",
+
+    cacheControl: function (url) {
+        // Caching enabled for .data and .bundle files.
+        // Revalidate if file is up to date before loading from cache
+        if (url.match(/\.data/)) {
+            return "must-revalidate";
+        }
+
+        if (url.match(/\.bundle/)) {
+            return "immutable";
+        }
+
+        // Caching enabled for .mp4 and .custom files
+        // Load file from cache without revalidation.
+        if (url.match(/\.mp4/) || url.match(/\.custom/)) {
+            return "immutable";
+        }
+
+        // Disable explicit caching for all other files.
+        // Note: the default browser cache may cache them anyway.
+        return "no-store";
+    },
+};
+
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+    // Define a maximum pixel ratio for mobile to avoid rendering at too high resolutions
+    const maxPixelRatioMobile = 2.0;
+    config.devicePixelRatio = Math.min(window.devicePixelRatio, maxPixelRatioMobile);
+}
+else {
+    config.devicePixelRatio = 2.0;
+}
+
+var canvas = document.querySelector("#unity-canvas");
+var loadingBar = document.querySelector("#unity-loading-fg");
+
+var unityGame;
+var script = document.createElement("script");
+script.src = loaderUrl;
+script.onload = function () {
+    createUnityInstance(canvas, config, function (progress) {
+        loadingBar.style.width = 75 * progress + "%";
+    }).then(function (unityInstance) {
+        unityGame = unityInstance;
+    }).catch(function (message) {
+        alert(message);
+    });
+};
+document.body.appendChild(script);
